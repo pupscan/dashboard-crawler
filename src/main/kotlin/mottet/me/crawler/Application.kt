2542983950c45856327b57b81c4dac5a497097e4
Thibaut Mottet <thibaut.mottet@7next.fr>
2017-07-13 18:42:21 +0200

    Enbal CORS all

--- src/main/kotlin/mottet/me/crawler/Application.kt
+++ src/main/kotlin/mottet/me/crawler/Application.kt
@@ -2,7 +2,10 @@ package mottet.me.crawler
 
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.context.annotation.Bean
 import org.springframework.scheduling.annotation.EnableScheduling
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 import java.text.DecimalFormat
 import java.text.DecimalFormatSymbols
 import java.time.LocalDateTime
@@ -12,7 +15,15 @@ import java.util.*
 
 @EnableScheduling
 @SpringBootApplication
-class CrawlerApplication
+class CrawlerApplication {
+    @Bean
+    fun corsConfigurer() =
+            object : WebMvcConfigurerAdapter() {
+                override fun addCorsMappings(registry: CorsRegistry) {
+                    registry.addMapping("/**").allowedOrigins("*")
+                }
+            }
+}
 
 fun main(args: Array<String>) {
     SpringApplication.run(CrawlerApplication::class.java, *args)
@@ -22,5 +33,6 @@ fun main(args: Array<String>) {
  * Util
  */
 fun Int.toReadableNumber() = DecimalFormat("#,###", DecimalFormatSymbols(Locale.FRANCE)).format(this)!!
+
 fun now() = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"))!!
 
