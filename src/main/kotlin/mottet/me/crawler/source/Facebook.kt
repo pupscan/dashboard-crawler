7aa59e07c5d9ccbac9a52b2176ead52c84f63fc8
Thibaut Mottet <thibaut.mottet@7next.fr>
2017-07-20 10:48:53 +0200

    Fix missing @Service

--- src/main/kotlin/mottet/me/crawler/source/Facebook.kt
+++ src/main/kotlin/mottet/me/crawler/source/Facebook.kt
@@ -9,6 +9,7 @@ import org.springframework.data.mongodb.core.index.Indexed
 import org.springframework.data.mongodb.core.mapping.Document
 import org.springframework.data.repository.CrudRepository
 import org.springframework.scheduling.annotation.Scheduled
+import org.springframework.stereotype.Service
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import java.time.LocalDate
@@ -28,6 +29,7 @@ class FacebookController(val service: FacebookService) {
             "\"${service.lastUpdateDateTime().toReadableDate()}\" }"
 }
 
+@Service
 class FacebookService(val repository: FacebookRepository) {
     private var favorites = 0
     private var followers = 0
