904df72f559f32e16d7b264eae3c812c73b45e21
Thibaut Mottet <thibaut.mottet@7next.fr>
2017-07-20 11:26:41 +0200

    Add current value at last 30 days for Twitter and Facebook

--- src/main/kotlin/mottet/me/crawler/source/Facebook.kt
+++ src/main/kotlin/mottet/me/crawler/source/Facebook.kt
@@ -1,6 +1,5 @@
 package mottet.me.crawler.source
 
-import mottet.me.crawler.now
 import mottet.me.crawler.toReadableDate
 import mottet.me.crawler.toReadableNumber
 import org.jsoup.Jsoup
@@ -41,7 +40,7 @@ class FacebookService(val repository: FacebookRepository) {
     fun currentFavorites() = fetch("div:eq(2)._2pi9._2pi2 ._4bl9  div").replace("[^\\d]".toRegex(), "").toInt()
     fun currentFollowers() = fetch("div:eq(3)._2pi9._2pi2 ._4bl9  div").replace("[^\\d]".toRegex(), "").toInt()
     fun lastUpdateDateTime() = lastUpdated
-    fun last30days() = repository.findTop30ByOrderByDateDesc().map { it.followers }
+    fun last30days() = repository.findTop30ByOrderByDateDesc().map { it.followers } + Facebook(date = lastUpdateDateTime().toLocalDate(), favorites = currentFavorites(), followers = currentFollowers())
 
     @Scheduled(fixedDelay = 700_000, initialDelay = 0)
     fun fetch() {
